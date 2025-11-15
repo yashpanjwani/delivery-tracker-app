fa5cea4ab770efceeb099a86b88dd7ba6a8fabc1
yashpanjwani <82490699+yashpanjwani@users.noreply.github.com>
2025-11-15 18:27:53 +0530

    Update API URL to Vercel backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // your backend URL
+  baseURL: "https://delivery-tracker-app-jmw3-7tlj9mew3.vercel.app",// your backend URL
 });
 
 export const createOrder = (orderData) => API.post("/orders", orderData);
